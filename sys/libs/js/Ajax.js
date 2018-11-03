7e17ddc18950d0f153e4e46a05e77ad747dd14d0
remizero <remizero@josephine>
2018-11-02 20:07:05 -0400

    Se comenzó a definir el código de la clase ajax

--- sys/libs/js/Ajax.js
+++ sys/libs/js/Ajax.js
@@ -1,6 +1,38 @@
 /**
  * 
  */
+class Ajax {
+
+  constructor () {
+    
+    this.xmlhttp = false;
+    try {
+
+      this.xmlhttp = new ActiveXObject ( "Msxml2.XMLHTTP" );
+
+    } catch ( e ) {
+
+      try {
+
+        this.xmlhttp = new ActiveXObject ( "Microsoft.XMLHTTP" );
+
+      } catch ( E ) {
+
+        this.xmlhttp = false;
+      }
+    }
+    if ( !this.xmlhttp && typeof XMLHttpRequest != 'undefined' ) {
+
+      this.xmlhttp = new XMLHttpRequest ();
+    }
+  }
+  
+  
+}
+
+
+
+
 function objetoAjax () {
 
   var xmlhttp = false;
