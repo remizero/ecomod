d298b882a68107a858b31441f7465e487913b5a1
Filiberto Zaá Avila <filizaa@gmail.com>
2018-11-04 01:47:28 -0400

    se agregaron algunos enlaces para definir correctamente la clase ajax

--- sys/libs/common/PhpAjaxBridge.php
+++ sys/libs/common/PhpAjaxBridge.php
@@ -1,5 +1,19 @@
 <?php
 
+require_once 'sys/core/ClassLoader.php';
 
+$classloader = new sys\core\ClassLoader ();
+$classloader->register ();
+
+use sys\core\Request;
+
+if ( Request::isXmlHttpRequest () ) {
+  
+  
+  
+} else {
+  
+  
+}
 
 ?>
--- sys/libs/js/Ajax.js
+++ sys/libs/js/Ajax.js
@@ -27,7 +27,44 @@ class Ajax {
     }
   }
   
-  
+  // PARA UTILIZAR
+  // https://es.stackoverflow.com/questions/138528/cómo-llamar-a-una-función-concreta-de-php-desde-ajax
+  // https://magmax.org/blog/ajax-principios-basicos/
+  // https://www.clubdelphi.com/foros/showthread.php?t=38993
+  // http://clubdelpra.cluster003.ovh.net/foros/showthread.php?p=176522&langid=1
+  // https://xhr.spec.whatwg.org/
+  // https://xhr.spec.whatwg.org/#security-considerations
+  // https://es.wikipedia.org/wiki/AJAX
+  // https://es.wikipedia.org/wiki/XMLHttpRequest
+  // 
+  // https://librosweb.es/libro/ajax/capitulo_7/la_primera_aplicacion.html
+  // http://www.forosdelweb.com/f68/poo-con-ajax-gran-duda-726880/
+  // https://www.phpclasses.org/browse/file/11788.html
+  // 
+}
+
+function isValidJson ( json ) {
+	
+  // https://es.stackoverflow.com/questions/133325/validar-un-json-e-imprimirlo-formateado
+	try {
+	  
+    objJson = JSON.parse ( json );
+    //console.log ( 'Sintaxis Correcta' );
+    
+	}	catch ( error ) {
+	  
+	  return false;
+    /*if ( error instanceof SyntaxError ) {
+      
+      let mensaje = error.message;
+      console.log ( 'ERROR EN LA SINTAXIS:', mensaje );
+        
+    } else {
+      
+      throw error; // si es otro error, que lo siga lanzando
+    }*/
+	}
+	return true;
 }
 
 
